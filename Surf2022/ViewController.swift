23844457d06afde4136ce5b0b86b9c09f7a3f136
Alex Petrenko <alex.pt@ya.ru>
2024-06-19 22:19:01 +0300

    Initial Commit

---
+++ Surf2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Surf2022
+//
+//  Created by Алексей on 19.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
